df69ea47360e01388f45201a7f1bf57d5b6b776e
Subc2 <subc2@wp.pl>
2015-09-22 18:24:51 +0200

    Added setmode(), clarified the code and help(module) output

--- RPiStepMotor.py
+++ RPiStepMotor.py
@@ -1,6 +1,6 @@
-"""RPiStepMotor - STEP MOTOR 28BYJ-48 driver for Raspberry Pi
+"""STEP MOTOR 28BYJ-48 driver for Raspberry Pi
 
-				Power----------------+
+        Power----------------+
 GPIOXX--------+              |
 GPIOXX------+ |              |
 GPIOXX----+ | |  +=========+ |
@@ -22,14 +22,18 @@ from __future__ import division, unicode_literals
 
 __author__ = "Paweł Zacharek"
 __copyright__ = "Copyright (C) 2015 Paweł Zacharek"
-__date__ = "2015-09-21"
+__date__ = "2015-09-22"
+__name__ = "RPiStepMotor"
 __license__ = "GPLv2+"
-__version__ = "0.7.5"
+__version__ = "0.7.6"
 
 import math
 import RPi.GPIO as GPIO
 import threading
 import time
+import warnings
+
+GPIO.defaultMode = GPIO.BCM
 
 allMotors = set()
 allPins = set()
@@ -51,16 +55,21 @@ class StepMotor(object):
 		fullRotation -- number of cycles needed to make a complete turn
 		"""
 		if len(pins) != phases:
-			raise WrongInputPinsNumber("step motor needs %d input pins" % phases)
+			raise ValueError("step motor needs %d input pins" % phases)
 		if [pin for pin in pins if pin in allPins]:
-			raise PinsAlreadyUsed("some pins are already in use")
+			raise ValueError("some pins are already in use")
 		global allMotors, allPins
 		allMotors.add(self)
 		allPins.update(pins)
 		self._fullRotation = fullRotation
 		self._pins = pins
 		self._thread = threading.Thread()
-		GPIO.setup(self._pins, GPIO.OUT, initial=False)
+		try:
+			GPIO.setup(self._pins, GPIO.OUT, initial=False)
+		except RuntimeError:
+			warnings.warn("pin numbering mode wasn't set, using default settings", RuntimeWarning)
+			self.setmode()
+			GPIO.setup(self._pins, GPIO.OUT, initial=False)
 	
 	def cleanup(self=None):
 		"""Perform a cleanup of stepper motor object(s).
@@ -74,19 +83,8 @@ class StepMotor(object):
 				or objects (if 'self' is a tuple, list or set)
 		"""
 		iterable = allMotors if self is None else self if type(self) in (tuple, list, set) else (self,)
-		for motor in iterable.copy():
-			motor.__cleanup__()
-	
-	def __cleanup__(self):
-		""" TODO """
-		global allMotors, allPins
-		if self.isRunning():
-			self.finish()
-		GPIO.cleanup(self._pins)
-		allPins.difference_update(self._pins)
-		del self._fullRotation
-		del self._pins
-		del self._thread
+		for motor in list(iterable):
+			motor._delete()
 	
 	def finish(self):
 		"""Wait till object's thread end."""
@@ -113,7 +111,7 @@ class StepMotor(object):
 		radians -- use radians instead of degrees
 		"""
 		if self.isRunning():
-			raise AlreadyRunning("step motor already running")
+			raise RuntimeError("step motor already running")
 		pins = self._pins[::-1] if angle < 0 else self._pins
 		angle = math.degrees(abs(angle)) if radians else abs(angle)
 		steps = int(angle / 360 * self._fullRotation)
@@ -122,6 +120,26 @@ class StepMotor(object):
 		else:
 			self._thread = threading.Thread(target=self._fullCycle, args=(pins, steps, timeForRevolution, function))
 			self._thread.start()
+	
+	@staticmethod
+	def setmode(mode=GPIO.defaultMode):
+		"""Set the pin numbering mode."""
+		GPIO.setmode(mode)
+	
+	def _delete(self):
+		"""Delete a single stepper motor object.
+		
+		It is always executed by cleanup().
+		"""
+		global allMotors, allPins
+		if self.isRunning():
+			self.finish()
+		GPIO.cleanup(self._pins)
+		allMotors.remove(self)
+		allPins.difference_update(self._pins)
+		del self._fullRotation
+		del self._pins
+		del self._thread
 	
 	def _fullCycle(self, pins, steps, timeForRevolution, function):
 		"""Function does all the dirty work in the process of rotation stepper motor.
@@ -150,7 +168,7 @@ class StepMotor(object):
 			stepDelay = timePeriod / max(cycles) / phases
 		
 		if stepDelay < minimalStepDelay:
-			raise StepDelayTooSmall("step delay is too small")
+			raise ValueError("step delay is too small")
 		
 		if function is None:
 			for i in range(steps):
@@ -169,15 +187,3 @@ class StepMotor(object):
 							GPIO.output(pin, True)
 							time.sleep(stepDelay)
 							GPIO.output(pin, False)
-
-class AlreadyRunning(RuntimeError):
-	pass
-
-class StepDelayTooSmall(ValueError):
-	pass
-
-class WrongInputPinsNumber(ValueError):
-	pass
-
-class PinsAlreadyUsed(ValueError):
-	pass
